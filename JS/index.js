7abd30cb456202756e3ad4e02d2c8979ff0c3cee
Mohamed Magdy <Eng.M.Ezzeldeen@gmail.com>
2023-11-30 22:24:40 +0200

    Add files via upload

--- JS/index.js
+++ JS/index.js
@@ -214,3 +214,34 @@ function openLink(url){
   return   window.open(`https://${url}`,"_blank")
 
 }
+
+
+document.addEventListener('contextmenu', (e) => e.preventDefault());
+
+function ctrlShiftKey(e, keyCode) {
+
+  return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
+
+}
+
+document.onkeydown = (e) => {
+
+  // Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
+
+  if (
+
+    e.key === 'F12' ||
+
+    ctrlShiftKey(e, 'I') ||
+
+    ctrlShiftKey(e, 'J') ||
+
+    ctrlShiftKey(e, 'C') ||
+
+    (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0))
+
+    )
+
+  return false;
+
+};
